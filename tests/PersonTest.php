fe3bd25dff115a683740a4e176feb7edb0aa06ee
NotAstronot <feririski409@gmail.com>
2023-10-01 01:35:48 +0700

    update

--- tests/PersonTest.php
+++ tests/PersonTest.php
@@ -6,35 +6,34 @@ use PHPUnit\Framework\TestCase;
 
 class PersonTest extends TestCase
 {
+    private Person $person;
 
-    // private Person $person;
+    protected function setUp(): void
+    {
+    }
 
-    // protected function setUp(): void
-    // {
-
-    // }
-
-    // /**
-    //  * @before
-    //  */
+    /**
+     * @before
+     */
+    public function createPerson()
+    {
+        $this->person = new Person("Astronot");
+    }
 
     public function testSucces()
     {
-        $person = new Person("Astronot");
-        self::assertEquals("Hello Boys, my name is Astronot", $person->sayHello("Boys"));
+        self::assertEquals("Hello Boys, my name is Astronot", $this->person->sayHello("Boys"));
     }
 
     public function testException()
     {
-        $person = new Person("Astronot");
         $this->expectException(\Exception::class);
-        $person->sayHello(null);
+        $this->person->sayHello(null);
     }
 
     public function testGoodByeSuccess()
     {
-        $person = new Person("Astronot");
         $this->expectOutputString("Good Bye Boys" . PHP_EOL);
-        $person->sayGoodBye("Boys");
+        $this->person->sayGoodBye("Boys");
     }
 }
