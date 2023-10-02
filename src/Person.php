d582f0822a9ad590d82c16ca13611f1673337fc9
NotAstronot <feririski409@gmail.com>
2023-10-02 23:57:09 +0700

    update

--- src/Person.php
+++ src/Person.php
@@ -20,4 +20,24 @@ class Person
     {
         echo "Good Bye $name" . PHP_EOL;
     }
+
+    /**
+     * Get the value of name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 }
